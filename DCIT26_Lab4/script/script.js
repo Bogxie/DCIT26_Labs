e4b06fc02a23a3735de88d82ceab590366006b85
Oriel Delos Reyes <delosreyes.oriel777@gmail.com>
2026-04-10 23:47:37 +0800

    Update script.js
    Cleaning Javascript code a bit

--- DCIT26_Lab4/script/script.js
+++ DCIT26_Lab4/script/script.js
@@ -1,9 +1,21 @@
-
 const display = document.querySelector('.display');
 
 display.addEventListener("input", () => {
     display.value = display.value.replace(/[^0-9+\-*/.]/g, '');
-})
+});
+
+const buttons = [
+    "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3", "/", "%", "0", ".", "C", "<-", "="
+];
+
+let buttonHTML = "";
+
+buttons.forEach((button) => {
+
+    buttonHTML += `<button class=${button === "=" ? "equals": ""}>${button}</button>`;
+});
+
+document.querySelector('.buttons').innerHTML = buttonHTML;
 
 document.querySelector('.buttons').addEventListener('click', (e) => {
     if (e.target.tagName !== 'BUTTON') return;
